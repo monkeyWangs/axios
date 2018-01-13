daed1482094524d2829d5bf44f92e92f8667a808
monkeyWangs <2424880409@qq.com>
2018-01-13 17:45:47 +0800

    add 校验

--- template/cover/index.js
+++ template/cover/index.js
@@ -3,6 +3,11 @@ import { convertRESTAPI } from '{{$$.relative("util")}}';
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
 function {{$$.convertMethod(mock)}}(opts) {
+  {{if (mock.parameters) {}}
+  if (!checkData(opts, {{JSON.stringify(mock.parameters)}}])) {
+    return
+  }
+  {{}}
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
@@ -13,3 +18,16 @@ function {{$$.convertMethod(mock)}}(opts) {
 <% }) %>export {<% _.forEach(data.mocks, function(mock, i){ %>
   {{$$.convertMethod(mock)}}<% if(data.mocks.length - 1 !== i) { %>,<% } %><% }) %>
 };
+
+function checkData (data, rules) {
+    let result = true
+    rules.forEach((rule) => {
+      if (rule.in === 'body' && rule.required) {
+        if (data[rule.name] === undefined) {
+          throw new Error(`${data[rule.name]} is required!`)
+          result = false
+        }
+      }
+    })
+    return result
+}
