66dbf273bda96fd5ed95b9ee4698daef4bf35c33
monkeyWangs <2424880409@qq.com>
2018-01-13 17:48:52 +0800

    add 校验

--- template/cover/index.js
+++ template/cover/index.js
@@ -3,11 +3,11 @@ import { convertRESTAPI } from '{{$$.relative("util")}}';
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
 function {{$$.convertMethod(mock)}}(opts) {
-  {{if (mock.parameters) {}}
+  <%if (mock.parameters) {%>
   if (!checkData(opts, {{JSON.stringify(mock.parameters)}}])) {
     return
   }
-  {{}}
+  <% } %>
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
