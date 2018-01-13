bc18f8fadf98b3953e13b067e42c648eac2e24f9
monkeyWangs <2424880409@qq.com>
2018-01-13 19:09:47 +0800

    add 校验

--- template/cover/rules.conf.js
+++ template/cover/rules.conf.js
@@ -1,5 +1,4 @@
 export default {<% _.forEach(data.mocks, function(mock, i){ %>
-  "{{mock.url}}": {{JSON.stringify(mock.parameters)}}
-  <% if(data.mocks.length - 1 !== i) { %>,<% } %>
+  "{{mock.url}}": {{JSON.stringify(mock.parameters)}}<% if(data.mocks.length - 1 !== i) { %>,<% } %>
 <% }) %>
 }
